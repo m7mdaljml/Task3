56ba7b94f83b39590dfa7e3d593d1b0bd0e1f682
Mohammed Al Jamal | CubesPlatform <mohammed.aljamal@cubesplatform.com>
2024-10-03 16:38:03 +0300

    Add New Features

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,35 @@
 <template>
   <v-navbar @onSelect="ChangeData" @onInput="SelectUser" />
-  <v-table :data="SearchResult" :schema="schema" />
+  <v-table :data="SearchResult" :schema="schema" @DeleteUser="deleteResult" @EditUser="editResult" />
+
+  <div v-if="Flag" class="modal-overlay">
+    <div class="modal-content">
+      <h3>Edit Record</h3>
+      <form @submit.prevent="saveEdit">
+        <div v-for="(value, key) in editedItem" :key="key">
+          <label>{{ key }}:</label>
+          <input v-model="editedItem[key]" :disabled="key === 'id'"/>
+        </div>
+        <button type="submit" class="btn btn-primary mt-3 p-2 m-1">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-floppy"
+            viewBox="0 0 16 16">
+            <path d="M11 2H9v3h2z" />
+            <path
+              d="M1.5 0h11.586a1.5 1.5 0 0 1 1.06.44l1.415 1.414A1.5 1.5 0 0 1 16 2.914V14.5a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 14.5v-13A1.5 1.5 0 0 1 1.5 0M1 1.5v13a.5.5 0 0 0 .5.5H2v-4.5A1.5 1.5 0 0 1 3.5 9h9a1.5 1.5 0 0 1 1.5 1.5V15h.5a.5.5 0 0 0 .5-.5V2.914a.5.5 0 0 0-.146-.353l-1.415-1.415A.5.5 0 0 0 13.086 1H13v4.5A1.5 1.5 0 0 1 11.5 7h-7A1.5 1.5 0 0 1 3 5.5V1H1.5a.5.5 0 0 0-.5.5m3 4a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 .5-.5V1H4zM3 15h10v-4.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5z" />
+          </svg>
+        </button>
+        <button type="button" class="btn btn-danger mt-3 p-2 m-1" @click="Flag = false">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-square"
+            viewBox="0 0 16 16">
+            <path
+              d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
+            <path
+              d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+          </svg>
+        </button>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -16,6 +45,8 @@ const EmpSchema = ref(['id', 'first_name', 'last_name', 'email', 'gender', 'ip_a
 const data = ref([])
 const schema = ref([])
 const SearchValue = ref('')
+const Flag = ref(false)
+const editedItem = ref({})
 
 const GetData = async () => {
   try {
@@ -42,23 +73,68 @@ const ChangeData = (obj) => {
 }
 
 const SelectUser = (text) => {
-  SearchValue.value = text.toLowerCase()
+  SearchValue.value = text.toLowerCase();
 }
 
 const SearchResult = computed(() => {
   if (!SearchValue.value) {
     return data.value
   }
-  return data.value.filter(item =>
-    Object.keys(item).some(key =>
-      String(item[key]).toLowerCase().includes(SearchValue.value)
-    )
+  return data.value.filter((item) =>
+    Object.keys(item).some((key) => String(item[key]).toLowerCase().includes(SearchValue.value))
   )
 })
+
+const deleteResult = (id) => {
+  data.value = data.value.filter((item) => item.id !== id)
+}
+
+const editResult = (id) => {
+  const itemToEdit = data.value.find(item => item.id === id)
+  if (itemToEdit) {
+    editedItem.value = { ...itemToEdit }
+    Flag.value = true
+  }
+}
+
+const saveEdit = () => {
+  const index = data.value.findIndex(item => item.id === editedItem.value.id)
+  if (index !== -1) {
+    data.value[index] = { ...editedItem.value }
+  }
+  Flag.value = false
+}
+
 
 onMounted(async () => {
   await GetData()
 })
 </script>
 
-<style scoped></style>
+<style scoped>
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal-content {
+  background-color: white;
+  padding: 20px;
+  border-radius: 5px;
+  width: 400px;
+}
+
+input {
+  display: block;
+  margin-bottom: 10px;
+  padding: 8px;
+  width: 100%;
+}
+</style>
--- src/components/table.vue
+++ src/components/table.vue
@@ -1,25 +1,33 @@
 <template>
     <div class="container mt-4">
-
-
         <div class="table-responsive">
             <table class="table table-striped">
                 <thead>
                     <tr>
-                        <th v-for="(key, index) in schema" :key="index">
-                            {{ key }}
-                        </th>
+                        <th v-for="(key, index) in schema" :key="index">{{ key }}</th>
                         <th colspan="2">Action</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr v-for="item in data.slice(first, end)" :key="item.id">
-                        <td v-for="(key, index) in schema" :key="index">
-                            {{ item[key] }}
-                        </td>
+                        <td v-for="(key, index) in schema" :key="index">{{ item[key] }}</td>
                         <td>
-                            <button class="btn btn-outline-secondary btn-sm me-2">Edit</button>
-                            <button class="btn btn-outline-danger btn-sm">Delete</button>
+                            <button class="btn btn-outline-secondary btn-sm me-2" @click="$emit('EditUser', item.id)">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                    <path
+                                        d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                    <path fill-rule="evenodd"
+                                        d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
+                                </svg>
+                            </button>
+                            <button class="btn btn-outline-danger btn-sm" @click="$emit('DeleteUser', item.id)">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-trash3" viewBox="0 0 16 16">
+                                    <path
+                                        d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
+                                </svg>
+                            </button>
                         </td>
                     </tr>
                 </tbody>
@@ -37,7 +45,7 @@
 
 <script setup lang="ts">
 import { defineProps, ref } from 'vue';
-import axios from 'axios';
+
 defineProps({
     data: {
         type: Array,
@@ -47,30 +55,31 @@ defineProps({
         type: Array,
         required: true
     }
-})
+});
+defineEmits([
+    'DeleteUser',
+    'EditUser'
+]);
 
-let first = ref(0)
-let end = ref(10)
+let first = ref(0);
+let end = ref(10);
 
 const increment = () => {
     if (end.value <= 990) {
-        first.value += 10
-        end.value += 10
+        first.value += 10;
+        end.value += 10;
     }
-}
+};
 const decrement = () => {
     if (first.value >= 10) {
-        first.value -= 10
-        end.value -= 10
+        first.value -= 10;
+        end.value -= 10;
     }
-}
-
+};
 </script>
 
 <style scoped>
-.table{
+.table {
     border: 1px solid lightgray;
-    border-radius: 95% 5% 96% 4% / 4% 94% 6% 96% ;
 }
-
 </style>
